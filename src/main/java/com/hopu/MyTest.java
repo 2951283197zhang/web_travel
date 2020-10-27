0890883228201a88f9a398c11634e4613b28ea22
username <2951283197@qq.com>
2020-10-27 19:54:44 +0800

    提交已更改的项目信息

--- src/main/java/com/hopu/MyTest.java
+++ src/main/java/com/hopu/MyTest.java
@@ -3,5 +3,6 @@ package com.hopu;
 public class MyTest {
     public static void main(String[] args){
         System.out.println("hello svn!!!");
+        System.out.println("i am zjh!!");
     }
 }
