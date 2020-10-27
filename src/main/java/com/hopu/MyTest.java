f9cce3ea92a6e45a71474d4af2ca39c6e94cd8be
username <2951283197@qq.com>
2020-10-27 20:13:44 +0800

    主干提交项目信息

--- src/main/java/com/hopu/MyTest.java
+++ src/main/java/com/hopu/MyTest.java
@@ -3,6 +3,6 @@ package com.hopu;
 public class MyTest {
     public static void main(String[] args){
         System.out.println("hello svn!!!");
-        System.out.println("i am zjh!!");
+        System.out.println("i am sunny!!");
     }
 }
